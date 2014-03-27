27cae2caab841c6600a4fc46d41f5ed2d16dcc50
gracece <guolyuan@gmail.com>
2014-03-28 00:16:06 +0800

    move to mongodb & add bootstrap

--- index.py
+++ index.py
@@ -6,9 +6,26 @@ import tornado.options
 import tornado.web
 import tornado.escape
 import os.path
+import pymongo
 
 from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers=[
+                (r"/", IndexHandler),
+                (r"/draw", DrawHandler),
+                (r"/clear", clear),
+                ]
+        settings = dict(
+                template_path=os.path.join(os.path.dirname(__file__), "templates"),
+                static_path=os.path.join(os.path.dirname(__file__), "static"),
+                debug=True,
+                )
+        conn = pymongo.Connection("localhost",27017)
+        self.db = conn["yichuan"]
+        tornado.web.Application.__init__(self,handlers,**settings)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -20,36 +37,24 @@ class DrawHandler(tornado.web.RequestHandler):
             timestamp = self.get_argument('t')
         except :
             timestamp = 0
-        fp = open("./line.txt","r")
-        drawLine = []
-        for line in fp:
-            line.split("#")
-            line = line.strip().split("#")
-            if line[0]>=timestamp:
-                data = dict()
-                data['timestamp'] = line[0]
-                data['line'] = tornado.escape.json_decode(line[1])
-                print data
-                drawLine.append(data)
-
-        self.write(tornado.escape.json_encode(drawLine))
+        coll = self.application.db.draw
+        drawLine = coll.find()
+        data = []
+        for line in drawLine:
+            print line
+            del line['_id']
+            data.append(line)
+        self.write(tornado.escape.json_encode(data))
 
 class clear(tornado.web.RequestHandler):
     def get(self):
-        fp = open("./line.txt","w")
+        coll = self.application.db.draw
+        coll.remove()
 
 
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[
-        (r"/", IndexHandler),
-        (r"/draw", DrawHandler),
-        (r"/clear", clear),
-        ],
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        )
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
--- server.py
+++ server.py
@@ -4,10 +4,13 @@ from SocketServer import(ThreadingTCPServer as TCP,StreamRequestHandler as SRH)
 from time import ctime
 import time
 import traceback
+import pymongo
 
 HOST = ''
 PORT = 10086
 ADDR = (HOST, PORT)
+conn = pymongo.Connection("localhost",27017)
+db = conn["yichuan"]
 
 class MyRequestHandler(SRH):
     def handle(self):
@@ -15,16 +18,17 @@ class MyRequestHandler(SRH):
             timestamp = int(time.time())
             try:
                 data = self.rfile.readline().strip().split(',')
-                print timestamp,self.client_address,data
-                record = '{"x1":%s,"y1":%s,"x2":%s,"y2":%s}'%(data[0],data[1],data[2],data[3])
-                fp = open('./line.txt','a')
-                fp.write(str(timestamp)+'#'+record+'\n')
+                coll  = db.draw
+                draw_line = {'timestamp':timestamp,'line':{'x1':int(data[0]),'y1':int(data[1]),\
+                        'x2':int(data[2]),'y2':int(data[3])}}
+                coll.insert(draw_line)
+                print timestamp,self.client_address,draw_line
             except :
                 print "break"
                 traceback.print_exc()
                 break
 
+TCP.allow_reuse_address = True
 tcpServ = TCP(ADDR,MyRequestHandler)
-tcpServ.allow_reuse_address=1
 print 'waitting for connection..'
 tcpServ.serve_forever()
